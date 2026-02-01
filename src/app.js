eadcbc96b340b948a3d90781dc5308b725e5942c
ALI <alijalal200311@gmail.com>
2026-02-01 11:13:24 +0300

    no changes

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ const app = express()
 if (NODE_ENV === 'development') {
   app.use(
     cors({
-      origin: 'http://localhost:5173',
+      origin: '"http://localhost:5173"',
       credentials: true,
       methods: ['GET', 'POST', 'PUT', 'DELETE'],
       allowedHeaders: ['Content-Type', 'Authorization']
